001ec52394ebb9ebceefe5dd9d7d88bb6a4112e8
Vimos Tan <vimostan@gmail.com>
2020-07-13 14:37:46 +0800

    add pretrain

--- docker_train.sh
+++ docker_train.sh
@@ -15,7 +15,12 @@ fi
 
 if [ -z "$CUDA_VISIBLE_DEVICES" ]; then
   CUDA_VISIBLE_DEVICES='all'
+  N_GPU=1
+else
+  N_GPU=`echo ${CUDA_VISIBLE_DEVICES} | tr -cd , | wc -c`
 fi
+
+echo "Training using ${N_GPU} GPUs: ${CUDA_VISIBLE_DEVICES}!"
 
 if [ -z "$CONFIG_FILE" ]; then
   CONFIG_FILE="train-${SUB_PROJECT}-base-1gpu.json"
@@ -32,5 +37,5 @@ docker run --gpus '"'device=$CUDA_VISIBLE_DEVICES'"' --ipc=host --rm -it \
   --mount src="$TXT_DB",dst=/txt,type=bind \
   -e NVIDIA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
   -w /src vimos/uniter_ve:latest \
-  bash -c " PYTHONPATH=/src ${MODEL_PARA} \\
+  bash -c " PYTHONPATH=/src ${MODEL_PARA} horovodrun -np $N_GPU \\
     python train_${SUB_PROJECT}.py --config=$CONFIG_DIR/$CONFIG_FILE"
