e0191f7592c351fc765b71018470199854fa549e
Vimos Tan <vimostan@gmail.com>
2020-09-14 20:26:16 +0800

    add contrastive

--- chengyubert/data/sampler.py
+++ chengyubert/data/sampler.py
@@ -5,7 +5,7 @@ Licensed under the MIT license.
 sampler for length bucketing (batch by tokens)
 """
 import random
-from collections import deque
+from collections import deque, Counter
 
 from cytoolz import partition_all
 from more_itertools import flatten
@@ -131,11 +131,12 @@ class ContrastivePairSampler(Sampler):
         self._rank = rank
         self._num_replicas = num_replicas
         self._lens = lens
-        self._ids = {v: k for k, v in enumerate(ids)}
+        self._ids = {v: k for k, v in enumerate(ids)}  # from example to example index
         self._max_tok = batch_size
         self._droplast = droplast
         self._size_mul = size_multiple
-        self.reverse_index = reverse_index
+        self.idiom_counter = Counter()
+        self.reverse_index = reverse_index  # from idiom id to example list
         self.contrastive_deque = deque(maxlen=500 * self._num_replicas)
 
     def _sort_fn(self, i):
@@ -143,16 +144,21 @@ class ContrastivePairSampler(Sampler):
 
     def _contrastive_bucket(self):
         if len(self.contrastive_deque) == 0:
-            self.contrastive_deque.append(random.choices(list(self.reverse_index.keys()),
-                                                         k=500 * self._num_replicas)[self._rank::self._num_replicas])
+            total = sum(self.idiom_counter.values()) + 1
+            weights = [1 - self.idiom_counter[i] / total for i in range(len(self.reverse_index))]
+            c = random.choices(range(len(self.reverse_index)),
+                               weights=weights,
+                               k=500 * self._num_replicas)
+            q = c[self._rank::self._num_replicas]
+            self.contrastive_deque.append(q)  # add sampled idiom ids
 
-        idiom_ids = self.contrastive_deque.pop()
+        idiom_ids = self.contrastive_deque.pop()  # pop idiom ids
 
         bucket = []
         for idx in random.choices(idiom_ids, k=500):
             try:
-                qid_pair = random.choices(self.reverse_index[idx], k=2)
-                bucket.append([self._ids[qid] for qid in qid_pair])
+                qid_pair = random.choices(self.reverse_index[idx], k=2)  # sample two examples given one idiom id
+                bucket.append([(idx, self._ids[qid]) for qid in qid_pair])  # convert example to example index
             except:
                 continue
         return bucket
@@ -163,7 +169,9 @@ class ContrastivePairSampler(Sampler):
             max_len = 0
             batch_indices = []
             for indices in partition_all(self._size_mul, bucket):
-                max_len = max(max_len, max(self._lens[j] for i in indices for j in i))
+                idiom_ids = [j for i in indices for j, _ in i]
+                indices = [j for i in indices for _, j in i]
+                max_len = max(max_len, max(self._lens[i] for i in indices))
                 if (max_len * (len(batch_indices) + self._size_mul) * 2
                         > self._max_tok):
                     if not batch_indices:
@@ -172,7 +180,8 @@ class ContrastivePairSampler(Sampler):
                     assert len(batch_indices) % self._size_mul == 0
                     break
                 else:
-                    batch_indices.extend(flatten(indices))
+                    batch_indices.extend(indices)
+                    self.idiom_counter.update(idiom_ids)
             assert len(batch_indices) % 2 == 0
             yield batch_indices
 
