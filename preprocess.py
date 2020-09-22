79551366a92a5193299a23b02ea5c7992d68d668
Vimos Tan <vimostan@gmail.com>
2020-09-22 14:12:54 +0800

    add balanced

--- preprocess.py
+++ preprocess.py
@@ -361,7 +361,7 @@ def main(opts):
     with open_db() as db:
         id2lens = process_chid(opts, db, tokenizer)
 
-    with open('/output/id2len.json', 'w') as f:
+    with open(f'{opts.output}/id2len.json', 'w') as f:
         json.dump(id2lens, f)
 
 
