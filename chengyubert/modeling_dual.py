c1bfc61daec74511a1368109f5feb4302a99da84
Vimos Tan <vimostan@gmail.com>
2020-10-09 20:21:29 +0800

    add fix

--- chengyubert/modeling_dual.py
+++ chengyubert/modeling_dual.py
@@ -24,7 +24,6 @@ class ChengyuBertSingle(BertPreTrainedModel):
 
     def forward(self, input_ids, token_type_ids, attention_mask, positions, option_ids,
                 inputs_embeds=None, options_embeds=None, compute_loss=False, targets=None):
-        # batch_size, sequence_num, length = input_ids.shape
         encoded_outputs = self.bert(input_ids,
                                     token_type_ids=token_type_ids,
                                     attention_mask=attention_mask,
@@ -33,7 +32,6 @@ class ChengyuBertSingle(BertPreTrainedModel):
 
         encoded_context = encoded_layer
         blank_states = encoded_context[[i for i in range(len(positions))], positions]  # [batch, hidden_state]
-        # cls_states = encoded_layer[:, 0]
 
         if option_ids is None and options_embeds is None:
             raise ValueError('Either option_ids or options_embeds should be given.')
@@ -43,13 +41,12 @@ class ChengyuBertSingle(BertPreTrainedModel):
             encoded_options = self.idiom_embedding(option_ids)  # (b, 10, 768)
 
         over_logits = self.vocab(blank_states)
-        # cond_logits = torch.gather(over_logits, dim=1, index=option_ids)
 
         mo_logits = torch.einsum('bld,bnd->bln', [encoded_context, encoded_options])  # (b, 256, 10)
         c_mo_logits, _ = torch.max(mo_logits, dim=1)
-        # over_states = cls_states
 
-        logits = c_mo_logits
+        c_fo_logits = torch.einsum('bd,bnd->bn', [blank_states, encoded_options])  # (b, 10)
+        logits = c_mo_logits + c_fo_logits
 
         if compute_loss:
             loss_fct = nn.CrossEntropyLoss()
@@ -69,19 +66,21 @@ class ChengyuBertDual(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
         emb_hidden_size = config.hidden_size
+        self.register_buffer('enlarged_candidates', torch.arange(len_idiom_vocab))
         self.idiom_facial_embedding = nn.Embedding(len_idiom_vocab, emb_hidden_size)
         self.idiom_meaning_embedding = nn.Embedding(len_idiom_vocab, emb_hidden_size)
 
         self.init_weights()
 
     def vocab(self, blank_states):
-        c_fo_logits = torch.einsum('bd,nd->bn', [blank_states, self.idiom_facial_embedding.weight])  # (b, 256, 10)
-        c_mo_logits = torch.einsum('bd,nd->bn', [blank_states, self.idiom_meaning_embedding.weight])  # (b, 256, 10)
+        idiom_facial_embeddings = self.idiom_facial_embedding(self.enlarged_candidates)
+        c_fo_logits = torch.einsum('bd,nd->bn', [blank_states, idiom_facial_embeddings])  # (b, 256, 10)
+        idiom_meaning_embeddings = self.idiom_embedding(self.enlarged_candidates)
+        c_mo_logits = torch.einsum('bd,nd->bn', [blank_states, idiom_meaning_embeddings])  # (b, 256, 10)
         return c_mo_logits + c_fo_logits
 
     def forward(self, input_ids, token_type_ids, attention_mask, positions, option_ids,
                 inputs_embeds=None, options_embeds=None, compute_loss=False, targets=None):
-        # batch_size, sequence_num, length = input_ids.shape
         encoded_outputs = self.bert(input_ids,
                                     token_type_ids=token_type_ids,
                                     attention_mask=attention_mask,
@@ -90,7 +89,6 @@ class ChengyuBertDual(BertPreTrainedModel):
 
         encoded_context = encoded_layer
         blank_states = encoded_context[[i for i in range(len(positions))], positions]  # [batch, hidden_state]
-        # cls_states = encoded_layer[:, 0]
 
         if option_ids is None and options_embeds is None:
             raise ValueError('Either option_ids or options_embeds should be given.')
@@ -101,11 +99,9 @@ class ChengyuBertDual(BertPreTrainedModel):
             meaning_state = self.idiom_meaning_embedding(option_ids)  # (b, 10, 768)
 
         over_logits = self.vocab(blank_states)
-        # cond_logits = torch.gather(over_logits, dim=1, index=option_ids)
 
         mo_logits = torch.einsum('bld,bnd->bln', [encoded_context, meaning_state])  # (b, 256, 10)
         c_mo_logits, _ = torch.max(mo_logits, dim=1)
-        # over_states = cls_states
 
         c_fo_logits = torch.einsum('bd,bnd->bn', [blank_states, facial_state])  # (b, 10)
         logits = c_mo_logits + c_fo_logits
