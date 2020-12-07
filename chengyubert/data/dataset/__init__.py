e550f409545faad7931df9f47c012c5ca8ebd9a8
Vimos Tan <vimostan@gmail.com>
2020-12-07 15:15:41 +0800

    add composition

---
+++ chengyubert/data/dataset/__init__.py
@@ -0,0 +1,46 @@
+import importlib
+import os
+
+DATA_REGISTRY = {}
+
+
+def register_dataset(name):
+    """
+    New model types can be added to fairseq with the :func:`register_model`
+    function decorator.
+
+    For example::
+
+        @register_model('lstm')
+        class LSTM(FairseqEncoderDecoderModel):
+            (...)
+
+    .. note:: All models must implement the :class:`BaseFairseqModel` interface.
+        Typically you will extend :class:`FairseqEncoderDecoderModel` for
+        sequence-to-sequence tasks or :class:`FairseqLanguageModel` for
+        language modeling tasks.
+
+    Args:
+        name (str): the name of the model
+    """
+
+    def register_model_cls(cls):
+        if name in DATA_REGISTRY:
+            raise ValueError('Cannot register duplicate model ({})'.format(name))
+        DATA_REGISTRY[name] = cls
+        return cls
+
+    return register_model_cls
+
+
+# automatically import any Python files in the models/ directory
+datasets_dir = os.path.dirname(__file__)
+for file in os.listdir(datasets_dir):
+    path = os.path.join(datasets_dir, file)
+    if (
+            not file.startswith('_')
+            and not file.startswith('.')
+            and (file.endswith('.py') or os.path.isdir(path))
+    ):
+        model_name = file[:file.find('.py')] if file.endswith('.py') else file
+        module = importlib.import_module(f'chengyubert.data.dataset.{model_name}')
