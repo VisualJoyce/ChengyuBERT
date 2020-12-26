b553d15db1899868d37fca5c72a2891e15d03f7e
Vimos Tan <vimostan@gmail.com>
2020-12-26 22:26:42 +0800

    Add layernorm

--- chengyubert/models/modeling_2stage.py
+++ chengyubert/models/modeling_2stage.py
@@ -17,12 +17,11 @@ class ChengyuBertTwoStagePretrain(BertPreTrainedModel):
 
         self.over_linear = nn.Linear(config.hidden_size * 4, config.hidden_size)
         self.idiom_embedding = nn.Embedding(len_idiom_vocab, config.hidden_size)
-        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.register_buffer('enlarged_candidates', torch.arange(len_idiom_vocab))
         self.init_weights()
 
     def vocab(self, blank_states):
-        idiom_embeddings = self.LayerNorm(self.idiom_embedding(self.enlarged_candidates))
+        idiom_embeddings = self.idiom_embedding(self.enlarged_candidates)
         return torch.einsum('bd,nd->bn', [blank_states, idiom_embeddings])  # (b, 256, 10)
 
     def forward(self, input_ids, token_type_ids, attention_mask, positions, option_ids,
@@ -61,6 +60,130 @@ class ChengyuBertTwoStageMaskPretrain(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
 
         self.idiom_embedding = nn.Embedding(len_idiom_vocab, config.hidden_size)
+        self.register_buffer('enlarged_candidates', torch.arange(len_idiom_vocab))
+        self.init_weights()
+
+    def vocab(self, blank_states):
+        idiom_embeddings = self.idiom_embedding(self.enlarged_candidates)
+        return torch.einsum('bd,nd->bn', [blank_states, idiom_embeddings])  # (b, 256, 10)
+
+    def forward(self, input_ids, token_type_ids, attention_mask, positions, option_ids,
+                inputs_embeds=None, options_embeds=None, compute_loss=False, targets=None):
+        encoded_outputs = self.bert(input_ids,
+                                    token_type_ids=token_type_ids,
+                                    attention_mask=attention_mask,
+                                    inputs_embeds=inputs_embeds)
+        encoded_layer = encoded_outputs[0]
+        blank_states = encoded_layer[[i for i in range(len(positions))], positions]  # [batch, hidden_state]
+
+        over_logits = self.vocab(blank_states)
+
+        if compute_loss:
+            loss_fct = nn.CrossEntropyLoss()
+            target = torch.gather(option_ids, dim=1, index=targets.unsqueeze(1))
+            over_loss = loss_fct(over_logits, target.squeeze(1))
+            return over_loss
+        else:
+            cond_logits = torch.gather(over_logits, dim=1, index=option_ids)
+            return cond_logits, over_logits
+
+
+@register_model('chengyubert-2stage-stage1-cls')
+class ChengyuBertTwoStageCLSPretrain(BertPreTrainedModel):
+    def __init__(self, config, len_idiom_vocab, model_name='chengyubert-2stage'):
+        super().__init__(config)
+        self.model_name = model_name
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+        self.over_linear = nn.Linear(config.hidden_size * 2, config.hidden_size)
+        self.idiom_embedding = nn.Embedding(len_idiom_vocab, config.hidden_size)
+        self.register_buffer('enlarged_candidates', torch.arange(len_idiom_vocab))
+        self.init_weights()
+
+    def vocab(self, blank_states):
+        idiom_embeddings = self.idiom_embedding(self.enlarged_candidates)
+        return torch.einsum('bd,nd->bn', [blank_states, idiom_embeddings])  # (b, 256, 10)
+
+    def forward(self, input_ids, token_type_ids, attention_mask, positions, option_ids,
+                inputs_embeds=None, options_embeds=None, compute_loss=False, targets=None):
+        encoded_outputs = self.bert(input_ids,
+                                    token_type_ids=token_type_ids,
+                                    attention_mask=attention_mask,
+                                    inputs_embeds=inputs_embeds)
+        encoded_layer = encoded_outputs[0]
+        blank_states = encoded_layer[[i for i in range(len(positions))], positions]  # [batch, hidden_state]
+        cls_states = encoded_layer[:, 0]
+
+        over_states = self.over_linear(torch.cat([blank_states,
+                                                  cls_states], dim=-1))
+
+        over_logits = self.vocab(over_states)
+
+        if compute_loss:
+            loss_fct = nn.CrossEntropyLoss()
+            target = torch.gather(option_ids, dim=1, index=targets.unsqueeze(1))
+            over_loss = loss_fct(over_logits, target.squeeze(1))
+            return over_loss
+        else:
+            cond_logits = torch.gather(over_logits, dim=1, index=option_ids)
+            return cond_logits, over_logits
+
+
+@register_model('chengyubert-layernorm-2stage-stage1')
+class ChengyuBertLayerNormTwoStagePretrain(BertPreTrainedModel):
+    def __init__(self, config, len_idiom_vocab, model_name='chengyubert-2stage'):
+        super().__init__(config)
+        self.model_name = model_name
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+        self.over_linear = nn.Linear(config.hidden_size * 4, config.hidden_size)
+        self.idiom_embedding = nn.Embedding(len_idiom_vocab, config.hidden_size)
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.register_buffer('enlarged_candidates', torch.arange(len_idiom_vocab))
+        self.init_weights()
+
+    def vocab(self, blank_states):
+        idiom_embeddings = self.LayerNorm(self.idiom_embedding(self.enlarged_candidates))
+        return torch.einsum('bd,nd->bn', [blank_states, idiom_embeddings])  # (b, 256, 10)
+
+    def forward(self, input_ids, token_type_ids, attention_mask, positions, option_ids,
+                inputs_embeds=None, options_embeds=None, compute_loss=False, targets=None):
+        encoded_outputs = self.bert(input_ids,
+                                    token_type_ids=token_type_ids,
+                                    attention_mask=attention_mask,
+                                    inputs_embeds=inputs_embeds)
+        encoded_layer = encoded_outputs[0]
+        blank_states = encoded_layer[[i for i in range(len(positions))], positions]  # [batch, hidden_state]
+        cls_states = encoded_layer[:, 0]
+
+        over_states = self.over_linear(torch.cat([blank_states,
+                                                  cls_states,
+                                                  blank_states * cls_states,
+                                                  blank_states - cls_states], dim=-1))
+
+        over_logits = self.vocab(over_states)
+
+        if compute_loss:
+            loss_fct = nn.CrossEntropyLoss()
+            target = torch.gather(option_ids, dim=1, index=targets.unsqueeze(1))
+            over_loss = loss_fct(over_logits, target.squeeze(1))
+            return over_loss
+        else:
+            cond_logits = torch.gather(over_logits, dim=1, index=option_ids)
+            return cond_logits, over_logits
+
+
+@register_model('chengyubert-layernorm-2stage-stage1-mask')
+class ChengyuBertLayerNormTwoStageMaskPretrain(BertPreTrainedModel):
+    def __init__(self, config, len_idiom_vocab, model_name):
+        super().__init__(config)
+        self.model_name = model_name
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+        self.idiom_embedding = nn.Embedding(len_idiom_vocab, config.hidden_size)
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
         self.register_buffer('enlarged_candidates', torch.arange(len_idiom_vocab))
         self.init_weights()
@@ -90,8 +213,8 @@ class ChengyuBertTwoStageMaskPretrain(BertPreTrainedModel):
             return cond_logits, over_logits
 
 
-@register_model('chengyubert-2stage-stage1-cls')
-class ChengyuBertTwoStageCLSPretrain(BertPreTrainedModel):
+@register_model('chengyubert-layernorm-2stage-stage1-cls')
+class ChengyuBertLayerNormTwoStageCLSPretrain(BertPreTrainedModel):
     def __init__(self, config, len_idiom_vocab, model_name='chengyubert-2stage'):
         super().__init__(config)
         self.model_name = model_name
