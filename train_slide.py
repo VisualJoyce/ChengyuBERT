e4f3530961cc4f0b0282cddb260ccfc1bd48f302
Vimos Tan <vimostan@gmail.com>
2021-05-15 17:12:14 +0800

    Add coattention

--- train_slide.py
+++ train_slide.py
@@ -69,7 +69,6 @@ def train(model, dataloaders, opts):
     model.train()
     n_examples = 0
     n_epoch = 0
-    last_valid = 0
     best_ckpt = 0
     best_eval = 0
     start = time()
@@ -157,16 +156,13 @@ def train(model, dataloaders, opts):
                             best_ckpt = global_step
                             best_eval = log['val/acc']
                             pbar.set_description(f'{opts.model}: {n_epoch}-{best_ckpt} best_acc-{best_eval * 100:.2f}')
-                            last_valid = global_step
                     model_saver.save(model, global_step)
             if global_step >= opts.num_train_steps:
                 break
-        if global_step >= opts.num_train_steps or global_step - last_valid > 0.1 * opts.num_train_steps:
+        if global_step >= opts.num_train_steps or global_step - best_ckpt > 0.1 * opts.num_train_steps:
             break
         n_epoch += 1
         LOGGER.info(f"Step {global_step}: finished {n_epoch} epochs")
-        # if n_epoch >= opts.num_train_epochs:
-        #     break
     return best_ckpt
 
 
